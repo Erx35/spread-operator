7c6dc4201cdba995942e63e6d9411d77779bd5f1
Ergo Kannikka <ergokannikka@gmail.com>
2024-06-11 19:27:40 +0000

    cade

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,16 @@
  */
 
 // No spread operator
-
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
 // Copying an array
-
+arr3 = [4, 5, 6];
+arr4 = [...arr3]
+console.log(arr4)
 // Copying an object
-
-// Copying only part of an array/object
+let obj = {a:1, b:2, c:3};
+let obj2 = {...obj, d:4};
+console.log(obj2);
+;// Copying only part of an array/object
+let arr5 = [...arr1, {...obj}, ...arr3, "x", "y", "z"];
+console.log(arr5);
